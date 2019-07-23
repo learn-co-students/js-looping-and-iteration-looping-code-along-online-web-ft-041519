ab463d3eaf9aee877296ff1d6efa6972b7bc072b
Erin Stalzer <erinstalzer@gmail.com>
2019-07-22 22:11:59 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names) {
+    let thankYou = []
+    for(let i = 0; i < names.length; i++) {
+        let thanks = "Thank you, " + names[i] + ", for the wonderful surprise gift!"
+        thankYou.push(thanks)
+    }
+    return thankYou
+}
+
+function countdown(num) {
+    for(let i = num; i > -1; i--) {
+        console.log(i)
+    }
+}
